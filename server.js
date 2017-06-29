5fc52ae41390f8e11a3811a04e4c288232bf150b
Joshua Flack <joshu.alexander@gmail.com>
2017-06-29 16:54:11 -0400

    almost there

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const fs = require('fs');
 
 const app = express();
 
+
+
 app.engine('mustache', mustacheExpress());
-app.set('views', './public');
+app.set('views', './views');
 app.set('view engine', 'mustache');
 
 app.use(bodyParser.urlencoded({
@@ -23,10 +25,88 @@ app.use(session({
   saveUninitialized: true
 }))
 
-app.get('/', function(req, res) {
-  res.render('game');
+app.use(function(req, res, next) {
+  if (!req.session.game) {
+    req.session.game = {};
+
+    req.session.game.words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+
+  }
+  next();
 })
 
-app.listen(3000, function(){
+
+app.get('/', function(req, res) {
+
+  if (!req.session.game.wordArr) {
+
+    const words = req.session.game.words;
+
+    let randWord = words[Math.floor(Math.random() * words.length)];
+
+    console.log(randWord);
+
+    let gameArr = [];
+
+    for (let idx = 0; idx < randWord.length; idx++) {
+      gameArr.push("_");
+    }
+
+    let wordArr = randWord.split("");
+
+    let deadLetters = [];
+
+    let gameLives = 8;
+
+    req.session.game.gameArr = gameArr;
+    req.session.game.wordArr = wordArr;
+    req.session.game.deadLetters = deadLetters;
+    req.session.game.gameLives = gameLives;
+
+    console.log(gameArr);
+    console.log(wordArr);
+    console.log(deadLetters);
+    console.log(gameLives);
+
+  }
+
+  res.render('game', req.session.game);
+
+})
+
+app.post('/', function(req, res) {
+
+  console.log(req.session.game.deadLetters);
+  const wordArr = req.session.game.wordArr;
+  const gameArr = req.session.game.gameArr;
+  let guessedLetter = req.body.playerGuess;
+
+  console.log(guessedLetter);
+
+  let deadLet = req.session.game.deadLetters;
+
+  if (deadLet.indexOf(guessedLetter) >= 0) {
+    console.log('You done goofed');
+    res.redirect('/');
+  } else {
+    for (let idx in wordArr) {
+      if (guessedLetter === wordArr[idx]) {
+        // req.session.game.gameArr.splice(idx, 1, guessedLetter);
+        gameArr[idx] = wordArr[idx];
+        console.log(gameArr);
+        if (deadLet.indexOf(guessedLetter) < 0) {
+          deadLet.push(guessedLetter);
+        }
+      }
+    }
+    if (wordArr.indexOf(guessedLetter) < 0) {
+      deadLet.push(guessedLetter);
+      req.session.game.gameLives--;
+    }
+  }
+  res.redirect('/');
+});
+
+app.listen(3000, function() {
   console.log("MYSTERY WORD online...");
 })
